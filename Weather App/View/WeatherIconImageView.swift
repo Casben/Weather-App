43df80eb4678b79a67442e153ad9854409a543f5
HDodge <herbiedodge@gmail.com>
2021-05-01 21:50:41 -0500

    Rework UI layout to be more fluid

---
+++ Weather App/View/WeatherIconImageView.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIconImageView.swift
+//  Weather App
+//
+//  Created by Herbert Dodge on 5/1/21.
+//
+
+import UIKit
+
+class WeatherIconImageView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 10
+    }
+
+}
